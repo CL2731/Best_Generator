4732c4876f952fd26f631b9fd5351df792c7f49d
Caitlin Lindauer <clilos018@gmail.com>
2022-03-31 19:19:55 -0600

    most of the start of all the necessary requirements

--- script.js
+++ script.js
@@ -1,14 +1,59 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var let1 = ["A", "B", ];
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+console.log(let1)
+var special = ["!", "$", "&", "+"];
+var ranchar = [];
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  //var password = generatePassword();
+ // var passwordText = document.querySelector("#password");
+  paslenth();
+  //passwordText.value = password;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function getusin (){
+    var lower= confirm ("Would you like lowercase letters included?")
+    var upper= confirm ("Would you like uppercase letters inculded?")
+    var bers = confirm ("Would you like numbers included?")
+    var spec = confirm ("Would you like special symbols included?")
+
+    if (!lower && !upper && !bers && !spec) {
+      alert("Must choose at least one.")
+    } else {
+      if (lower === true){
+        ranchar.push(letters)
+        console.log(ranchar)
+      } 
+      if (upper === true){
+        ranchar.push(let1)
+      }
+      if (bers === true){
+        ranchar.push(numbers)
+      }
+      if (spec === true){
+        ranchar.push(special)
+      } 
+      console.log(ranchar)
+    }
+}
+
+function paslenth (){
+  var userinput = prompt("How many characters would you like your password to be?\nMust be at least 8 and no more than 128.")
+  if (userinput > 7 && userinput < 129) {
+    getusin();
+  } else if (userinput <= 7 || userinput >= 129) {
+    alert("You have not entered valid information.")
+  } else if (userinput === null) {
+    alert("Invalid input.");
+  }
+}
+
+
