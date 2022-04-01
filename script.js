741b74f776cc1638d657be242645e8141b26af1b
Caitlin Lindauer <clilos018@gmail.com>
2022-03-31 22:37:25 -0600

    minor changes while trying to get the right output

--- script.js
+++ script.js
@@ -7,19 +7,18 @@ var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 console.log(let1);
 var special = ["!", "$", "&", "+"];
 var ranchar = "";
-var userinput = 0;
 // Write password to the #password input
 function writePassword() {
  var passwordText = document.querySelector("#password");
   paslenth();
   ranchar
-  //passwordText.value = password;
+  passwordText.value = password;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function getusin (){
+function getusin (userinput){
     var lower= confirm ("Would you like lowercase letters included?")
     var upper= confirm ("Would you like uppercase letters inculded?")
     var bers = confirm ("Would you like numbers included?")
@@ -42,17 +41,18 @@ function getusin (){
         ranchar= ranchar.concat(special)
       } 
       console.log(ranchar)
-    } console.log(userinput)
+    } 
 }
 
-function paslenth (){
-  userinput = prompt("How many characters would you like your password to be?\nMust be at least 8 and no more than 128.")
+function paslenth (userinput){
+   var userinput = prompt("How many characters would you like your password to be?\nMust be at least 8 and no more than 128.")
   if (userinput > 7 && userinput < 129) {
-    getusin();
+    getusin(userinput);
   } else if (userinput <= 7 || userinput >= 129) {
     alert("You have not entered valid information.")
   } else if (userinput === null) {
     alert("Invalid input.");
   }
- var  = Math.floor(Math.random() * ranchar)
+  console.log(userinput)
+  password = (Math.floor(Math.random()*(paslenth + getusin)))
 }
